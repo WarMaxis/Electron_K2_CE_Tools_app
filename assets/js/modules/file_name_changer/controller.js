5722e3d466b0f4b16fc5d25b7c342547dfd0bbbb
MICHAL-VM\warmaxis <michalmilanowski@op.pl>
2017-04-18 10:31:27 +0200

    Code refactor on File Name Changer module controller

--- assets/js/modules/file_name_changer/controller.js
+++ assets/js/modules/file_name_changer/controller.js
@@ -13,6 +13,13 @@ const appObjects = {
     alertContainerSecond: document.getElementById('alert-container-second')
 };
 
+// HTML objects markup
+const htmlMarkup = {
+    chooseFilesAlert: '<div id="choose-files-alert" class="alert alert-warning alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Nie wybrałeś żadnego folderu!</strong></div>',
+    appSuccessAlert: '<div id="app-success-alert" class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Zmiana nazw plików zakończona!</strong></div>'
+
+};
+
 // Work files directory
 var filesDirectory;
 
@@ -23,22 +30,30 @@ function chooseFiles() {
     }, function (folderPaths) {
         // folderPaths is an array that contains all the selected paths
         if (folderPaths === undefined) {
+            document.getElementById('alert-container-first').innerHTML += htmlMarkup.chooseFilesAlert;
+
             appObjects.chooseFilesAlert[0].style.display = 'block';
-            document.getElementById('alert-container-first').innerHTML += '<div id="choose-files-alert" class="alert alert-warning alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Nie wybrałeś żadnego folderu!</strong></div>';
             appObjects.filesQuantity.style.display = 'none';
+
             appObjects.startApp.setAttribute('disabled', 'disabled');
+
             return;
         } else {
             filesDirectory = folderPaths[0];
+
             appObjects.startApp.removeAttribute('disabled');
+
             recursive(filesDirectory, function (error, allFiles) {
                 appObjects.filesQuantity.innerHTML = allFiles.length;
+
                 allFiles.forEach(function (fileName, fileNumber) {
                     fileNumber += 1;
                     appObjects.filesTable.innerHTML += '<tr><th scope="row">' + fileNumber + '</th><td>' + fileName + '</td></tr>';
                 });
             });
+
             appObjects.filesQuantity.style.display = 'block';
+
             return;
         }
     });
@@ -46,21 +61,29 @@ function chooseFiles() {
 
 // Success rename and copy files
 function successRenameAndCopy() {
-    appObjects.appSuccessAlert[0].style.display = 'block';
     appObjects.filesTable.innerHTML = '';
+
+    appObjects.appSuccessAlert[0].style.display = 'block';
     appObjects.filesQuantity.style.display = 'none';
+
     appObjects.startApp.setAttribute('disabled', 'disabled');
+
+    return;
 }
 
 // Choose files to rename
 appObjects.chooseFiles.onclick = function () {
-    appObjects.alertContainerSecond.innerHTML += '<div id="app-success-alert" class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Zmiana nazw plików zakończona!</strong></div>';
+    appObjects.alertContainerSecond.innerHTML += htmlMarkup.appSuccessAlert;
+    appObjects.filesTable.innerHTML = '';
+
     appObjects.appSuccessAlert[0].style.display = 'none';
     appObjects.chooseFilesAlert[0].style.display = 'none';
-    appObjects.startApp.setAttribute('disabled', 'disabled');
     appObjects.filesQuantity.style.display = 'none';
-    appObjects.filesTable.innerHTML = '';
+
+    appObjects.startApp.setAttribute('disabled', 'disabled');
+
     console.clear();
+
     return chooseFiles();
 };
 
