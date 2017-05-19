293031c2a03384616ae2b35ee0cc0efbea0e687e
MICHAL-VM\warmaxis <michalmilanowski@op.pl>
2017-05-19 16:18:28 +0200

    Added NPM packages for Images Downloader module

--- assets/js/imports.js
+++ assets/js/imports.js
@@ -5,4 +5,10 @@
 const fse = require('fs-extra'),
     recursive = require('recursive-readdir'),
     app = require('electron').remote,
-    dialog = app.dialog;
+    dialog = app.dialog,
+
+    request = require('request'),
+    progress = require('request-progress'),
+    imagemin = require('imagemin'),
+    imageminMozjpeg = require('imagemin-mozjpeg'),
+    imageminPngquant = require('imagemin-pngquant');
